5b90b7f36e10bfa294fcb1ec30b42477e378aa53
liupo.wu <liupo.wu@wz-inc.com>
2020-09-18 15:23:34 +0800

    fix: 返回上一级

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mime = require('./mime');
 const baseDir = './dist';
 
 http.createServer((req, res) => {
-    const url = req.url.replace(/^\/|\/$/, '').replace(/\/+/g, '/').replace(/\.+/g, '.');
+    const url = req.url.replace(/^\/|\/$/g, '').replace(/\/+/g, '/').replace(/\.+/g, '.');
     let file = path.resolve(__dirname, baseDir, url);
     if (!fs.existsSync(file)) { // 文件不存在时，默认返回根目录
         res.statusCode = 302;
@@ -25,12 +25,18 @@ http.createServer((req, res) => {
     } else {    // 请求的目录
         res.setHeader('Content-Type', mime.html);
         const folder = url ? `/${url}/` : '/';
-        fs.readdir(file, {}, function (err, files) {
-            let rst = '<h1>Simple Node Server</h1><ol>'
-            rst += files.map(item => `<li><a href="${folder}${item}">${item}</a></li>`).join('');
-            rst += '</ol>';
-            res.end(rst);
-        });
+        let rst = `<h1>Simple Node Server: ${url}</h1><ol>`;
+
+        // 子目录时，可以返回上一级
+        if (url) {
+            rst += `<li><a href="${folder}..">..</a></li>`;
+        }
+
+        // 遍历所有子目录，并输出出来
+        files = fs.readdirSync(file, {});
+        rst += files.map(item => `<li><a href="${folder}${item}">${item}</a></li>`).join('');
+        rst += '</ol>';
+        res.end(rst);
     }
 }).listen(8000, function() {
     console.log('Serving on localhost:8000');
