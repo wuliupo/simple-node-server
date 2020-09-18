4b96b3c1839d972622ab1ba3c43569f3d329701b
liupo.wu <liupo.wu@wz-inc.com>
2020-09-18 16:44:27 +0800

    feat: 文件缓存等

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
 
 a. node.js 底层 api：const fs = require('fs');  http://nodejs.cn/api/fs.html 根据请求分两种：文件 + 目录
 b. http api: http://nodejs.cn/api/http.html#http_class_http_server
+
+## 功能点
+
+1. 文件内容查看
+2. 目录浏览
+3. 缓存（文件修改日期等）
+4. 前后端分离（API + 页面）
+5. 安全策略（磁盘文件权限、网页访问权限）
+6. 中文乱码
+7. 默认索引（可配置）
+8. 服务器端渲染（模块渲染）
+9. 性能（文件比较大）
+10. 跨域（对外提供 API，jsonp等）
+11. 参数配置（cli）
--- index.js
+++ index.js
@@ -7,37 +7,52 @@ const mime = require('./mime');
 const baseDir = './dist';
 
 http.createServer((req, res) => {
-    const url = req.url.replace(/^\/|\/$/g, '').replace(/\/+/g, '/').replace(/\.+/g, '.');
+    const url = decodeURIComponent(req.url)
+        .replace(/^\/|\/$/g, '')
+        .replace(/\/+/g, '/')
+        .replace(/\.+/g, '.');
     let file = path.resolve(__dirname, baseDir, url);
-    if (!fs.existsSync(file)) { // 文件不存在时，默认返回根目录
+    if (!fs.existsSync(file)) {
+        // 文件不存在时，默认返回根目录
         res.statusCode = 302;
         res.setHeader('Location', '/');
         res.end();
-    } else if (fs.statSync(file).isFile()) {    // 请求的是文件
+    } else if (fs.statSync(file).isFile()) {
+        // 请求的是文件
         let suffix = file.replace(/.*\./, '').toLowerCase();
         // 设置请求的返回头 type
         res.setHeader('Content-Type', mime[suffix] || mime.txt);
-        // 格式必须为 binary 否则会出错
-        const content = fs.readFileSync(file, 'binary');
-        res.writeHead(200, 'Ok');
-        res.write(content, 'binary'); // 格式必须为 binary，否则会出错
-        res.end();
-    } else {    // 请求的目录
+        // 建立流对象，读文件
+        const stream = fs.createReadStream(file);
+        // 错误处理
+        stream.on('error', function () {
+            res.writeHead(500, { 'content-type': mime.html });
+            res.end('<h1>500 Server Error</h1>');
+        });
+        stream.pipe(res);
+    } else {
+        // 请求的目录
         res.setHeader('Content-Type', mime.html);
         const folder = url ? `/${url}/` : '/';
-        let rst = `<h1>Simple Node Server: ${url}</h1><ol>`;
-
+        let rst = `<!doctype html>
+<html lang="zh-CN">
+    <head>
+    <meta charset="utf-8"/>
+    <title>Simple Node Server</title>
+</head>
+<body>
+    <h1>Simple Node Server: ${url}</h1>
+    <ol>`;
         // 子目录时，可以返回上一级
         if (url) {
             rst += `<li><a href="${folder}..">..</a></li>`;
         }
-
         // 遍历所有子目录，并输出出来
         files = fs.readdirSync(file, {});
-        rst += files.map(item => `<li><a href="${folder}${item}">${item}</a></li>`).join('');
+        rst += files.map((item) => `<li><a href="${folder}${item}">${item}</a></li>`).join('');
         rst += '</ol>';
         res.end(rst);
     }
-}).listen(8000, function() {
-    console.log('Serving on localhost:8000');
+}).listen(8000, function () {
+    console.log('Serving http://localhost:8000');
 });
